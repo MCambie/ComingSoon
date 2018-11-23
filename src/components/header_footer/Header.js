78479a7ebe66b2684b4d468dd80f8cdac2296e88
MCambie <cambie.marie@gmail.com>
2018-11-23 15:50:17 +0100

    'highlights'

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -45,7 +45,7 @@ toggleDrawer = (value) =>{
       >
           <Toolbar>
             <div className='header_logo'>
-              <div className="font_righteous header_logo_venue">The Venue</div>
+              <div className="font_righteous header_logo_venue">Coming Soon... </div>
               <div className="header_logo_title">Musical Events</div>
             </div>
             <IconButton
---
+++ src/components/highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className='center_wrapper'>
+          <h2>HIGHLIGHTS</h2>
+          <div className='highlight_description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius id velit non cursus. Donec consequat tellus vel accumsan efficitur. Proin tincidunt in mauris non porttitor. Curabitur dictum fringilla tincidunt. Vivamus aliquet lacinia cursus. Praesent sit amet faucibus odio, et sagittis metus. Morbi faucibus massa sed risus ultrices, at congue ex laoreet.
+          </div>
+      </div>
+    </Fade>
+  )
+}
+
+export default Description
